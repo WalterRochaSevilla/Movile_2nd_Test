6997716d34e583337600cea5c7e592adec917c83
Walter Rocha <walterrochasevilla@gmail.com>
2025-09-27 07:38:40 -0400

    Proyecto Base Clonado del repo osficial

---
+++ app/src/main/java/com/calyrsoft/ucbp1/navigation/Screen.kt
@@ -0,0 +1,11 @@
+package com.calyrsoft.ucbp1.navigation
+
+sealed class Screen(val route: String) {
+    object Home: Screen("home")
+    object Github: Screen("github")
+    object Profile: Screen("profile")
+
+    object CardExamples: Screen("card")
+    object Dollar: Screen("dollar")
+    object PopularMovies: Screen("popularMovies")
+}
